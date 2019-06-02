7b14d1a1dd6717a9001ee6db051967dc29b82326
Arnelle Balane <arnellebalane@gmail.com>
2019-06-03 00:55:55 +0800

    Add PartnersBlock to Home route

---
+++ src/components/partners-block/index.js
@@ -0,0 +1,30 @@
+import style from './style';
+import partners from './partners.json';
+
+const Partner = ({partner}) => (
+    <a href={partner.url} target="_blank" rel="noopener noreferrer">
+        <img src={partner.logo} />
+    </a>
+);
+
+const PartnersGroup = ({group}) => (
+    <div class={style.group}>
+        <h3>{group.label}</h3>
+
+        {group.items.map(partner => (
+            <Partner partner={partner} />
+        ))}
+    </div>
+);
+
+const PartnersBlock = () => (
+    <section class={style.partners}>
+        <h2>Partners</h2>
+
+        {partners.map(group => (
+            <PartnersGroup group={group} />
+        ))}
+    </section>
+);
+
+export default PartnersBlock;
--- src/routes/home/index.js
+++ src/routes/home/index.js
@@ -2,6 +2,7 @@ import { h, Component } from 'preact';
 import HeroBlock from '../../components/hero-block';
 import InfoBlock from '../../components/info-block';
 import PhotoGrid from '../../components/photo-grid';
+import PartnersBlock from '../../components/partners-block';
 import style from './style';
 
 export default class Home extends Component {
@@ -11,6 +12,7 @@ export default class Home extends Component {
                 <HeroBlock />
                 <InfoBlock />
                 <PhotoGrid />
+                <PartnersBlock />
             </div>
         );
     }
