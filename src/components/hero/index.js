640d300a5b0c6ee040d4b19e3887339baec0fd50
Arnelle Balane <arnellebalane@gmail.com>
2019-06-02 18:50:59 +0800

    Use LayoutGrid in Hero component

--- src/components/hero/index.js
+++ src/components/hero/index.js
@@ -1,4 +1,5 @@
 import { Component } from 'preact';
+import LayoutGrid from 'preact-material-components/LayoutGrid';
 import logo from '../../images/io.svg';
 import heroImage from '../../images/hero-image.jpg';
 import style from './style';
@@ -6,19 +7,21 @@ import style from './style';
 export default class Hero extends Component {
     render() {
         return (
-            <section class={style.hero}>
-                <div class={style.details}>
-                    <img class={style.logo} src={logo} alt="Google I/O Logo" />
-                    <h2 class={style.description}>
-                        I/O brings together developers from around the globe for talks, hands-on
-                        learning with Google experts, and a first look at Google’s latest developer products.
-                    </h2>
-                </div>
+            <LayoutGrid class={style.hero}>
+                <LayoutGrid.Inner>
+                    <LayoutGrid.Cell tabletCols="12" desktopCols="5" align="middle" class={style.details}>
+                        <img class={style.logo} src={logo} alt="Google I/O Logo" />
+                        <h2 class={style.description}>
+                            I/O brings together developers from around the globe for talks, hands-on
+                            learning with Google experts, and a first look at Google’s latest developer products.
+                        </h2>
+                    </LayoutGrid.Cell>
 
-                <div class={style.heroImage}>
-                    <img src={heroImage} alt="Google I/O Extended Cebu 2019" />
-                </div>
-            </section>
+                    <LayoutGrid.Cell tabletCols="12" desktopCols="6" align="middle" class={style.heroImage}>
+                        <img src={heroImage} alt="Google I/O Extended Cebu 2019" />
+                    </LayoutGrid.Cell>
+                </LayoutGrid.Inner>
+            </LayoutGrid>
         );
     }
 }
