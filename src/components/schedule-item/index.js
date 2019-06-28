c985836bf635a07d0634f51e8c288a2ea751b28e
Arnelle Balane <arnellebalane@gmail.com>
2019-06-28 22:33:15 +0800

    Display speakers in ScheduleItem components

--- src/components/schedule-item/index.js
+++ src/components/schedule-item/index.js
@@ -1,5 +1,6 @@
 import style from './style';
 import sessions from '../../data/sessions.json';
+import speakers from '../../data/speakers.json';
 
 const ScheduleItem = ({session}) => {
     const data = sessions[session.session];
@@ -11,6 +12,20 @@ const ScheduleItem = ({session}) => {
         >
             <h2 class={style.title}>{data.title}</h2>
             <p class={style.duration}>{session.duration}</p>
+
+            {data.speakers && (
+                <div class={style.speakers}>
+                    {data.speakers.map(speakerKey => {
+                        const speaker = speakers[speakerKey];
+                        return (
+                            <div class={style.speaker}>
+                                <img class={style.speakerPhoto} src={speaker.photo} alt={speaker.name} />
+                                <h4 class={style.speakerName}>{speaker.name}</h4>
+                            </div>
+                        );
+                    })}
+                </div>
+            )}
         </div>
     );
 };
