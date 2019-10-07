e316831318bb4088252e74dce00cd7e9fbe5187f
Arnelle Balane <arnellebalane@gmail.com>
2019-10-08 00:29:59 +0800

    Update HeroBlock component contents

--- src/components/hero-block/index.js
+++ src/components/hero-block/index.js
@@ -1,22 +1,18 @@
 import style from './style';
 import ButtonLink from '../button-link';
-import logo from '../../images/io.svg';
+import logo from '../../images/devfest-logo.svg';
+import event from '../../data/event.json';
 
 const HeroBlock = () => (
     <section class={style.hero}>
         <div class={style.details}>
-            <img class={style.logo} src={logo} alt="Google I/O Logo" />
-            <h2>
-                I/O brings together developers from around the globe for talks, hands-on
-                learning with Google experts, and a first look at Google’s latest developer products.
-            </h2>
+            <img src={logo} alt={event.name} />
 
-            <h3>July 20, 2019 &middot; J Centre, Mandaue City</h3>
+            <h2>{event.description}</h2>
+            <h3>{event.date} <span>&middot;</span> {event.venue}</h3>
 
-            <ButtonLink href="https://www.eventbrite.com/e/google-io-extended-cebu-2019-tickets-63569676614">Register Now</ButtonLink>
+            <ButtonLink href={event.register_url}>Register Now</ButtonLink>
         </div>
-
-        <img class={style.heroImage} src="/assets/hero-image.jpg" alt="Google I/O Extended Cebu 2019" />
     </section>
 );
 
