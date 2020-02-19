7157e0bf19f8cf0b1d6260ce1a3e7ebf2e68e738
Arnelle Balane <arnellebalane@gmail.com>
2020-02-19 14:26:55 +0800

    Update event data

--- src/components/hero-block/index.js
+++ src/components/hero-block/index.js
@@ -1,19 +1,19 @@
 import style from './style';
 import ButtonLink from '../button-link';
-import logo from '../../images/devfest-logo.svg';
+import logo from '../../images/wtm-logo.svg';
 import event from '../../data/event.json';
 
 const HeroBlock = () => (
-    <section class={style.hero}>
-        <div class={style.details}>
-            <img src={logo} alt={event.name} />
+	<section class={style.hero}>
+		<div class={style.details}>
+			<img src={logo} alt={event.name} />
 
-            <h2>{event.description}</h2>
-            <h3>{event.date} <span>&middot;</span> {event.venue}</h3>
+			<h2>{event.description}</h2>
+			<h3>{event.date} <span>&middot;</span> {event.venue}</h3>
 
-            <ButtonLink href={event.register_url}>Register Now</ButtonLink>
-        </div>
-    </section>
+			<ButtonLink href={event.register_url}>Register Now</ButtonLink>
+		</div>
+	</section>
 );
 
 export default HeroBlock;
