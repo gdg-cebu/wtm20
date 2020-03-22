8e9a8d393857143f7444b49d7095ac6ac86ebbae
Arnelle Balane <arnellebalane@gmail.com>
2020-03-22 23:14:29 +0800

    Use cloudinary demo account to serve images

--- src/components/photo-grid/index.js
+++ src/components/photo-grid/index.js
@@ -5,7 +5,7 @@ import style from './style';
 const prod = process.env.NODE_ENV === 'production';
 
 const getPhotoUrl = (path, transforms) => {
-    const cloudinaryUrl = 'https://res.cloudinary.com/arnellebalane/image/fetch';
+    const cloudinaryUrl = 'https://res.cloudinary.com/demo/image/fetch';
     const siteUrl = 'https://wtm.gdgcebu.org';
 
     const segments = [
@@ -20,36 +20,36 @@ const getPhotoUrl = (path, transforms) => {
 const PhotoGrid = () => (
     <div class={style.photoGrid}>
         <picture>
-            <source src={getPhotoUrl('/assets/gallery/01.jpg', 'w_560,f_webp,q_85')} type="image/webp" />
-            <img src={getPhotoUrl('/assets/gallery/01.jpg', 'w_560,q_85')} />
+            <source src={getPhotoUrl('assets/gallery/01.jpg', 'w_560,f_webp,q_85')} type="image/webp" />
+            <img src={getPhotoUrl('assets/gallery/01.jpg', 'w_560,q_85')} />
         </picture>
         <picture>
-            <source src={getPhotoUrl('/assets/gallery/02.jpg', 'w_560,f_webp,q_85')} type="image/webp" />
-            <img src={getPhotoUrl('/assets/gallery/02.jpg', 'w_560,q_85')} />
+            <source src={getPhotoUrl('assets/gallery/02.jpg', 'w_560,f_webp,q_85')} type="image/webp" />
+            <img src={getPhotoUrl('assets/gallery/02.jpg', 'w_560,q_85')} />
         </picture>
         <picture>
-            <source src={getPhotoUrl('/assets/gallery/03.jpg', 'w_560,f_webp,q_85')} type="image/webp" />
-            <img src={getPhotoUrl('/assets/gallery/03.jpg', 'w_560,q_85')} />
+            <source src={getPhotoUrl('assets/gallery/03.jpg', 'w_560,f_webp,q_85')} type="image/webp" />
+            <img src={getPhotoUrl('assets/gallery/03.jpg', 'w_560,q_85')} />
         </picture>
         <picture>
-            <source src={getPhotoUrl('/assets/gallery/04.jpg', 'w_560,f_webp,q_85')} type="image/webp" />
-            <img src={getPhotoUrl('/assets/gallery/04.jpg', 'w_560,q_85')} />
+            <source src={getPhotoUrl('assets/gallery/04.jpg', 'w_560,f_webp,q_85')} type="image/webp" />
+            <img src={getPhotoUrl('assets/gallery/04.jpg', 'w_560,q_85')} />
         </picture>
         <picture>
-            <source src={getPhotoUrl('/assets/gallery/05.jpg', 'w_560,f_webp,q_85')} type="image/webp" />
-            <img src={getPhotoUrl('/assets/gallery/05.jpg', 'w_560,q_85')} />
+            <source src={getPhotoUrl('assets/gallery/05.jpg', 'w_560,f_webp,q_85')} type="image/webp" />
+            <img src={getPhotoUrl('assets/gallery/05.jpg', 'w_560,q_85')} />
         </picture>
         <picture>
-            <source src={getPhotoUrl('/assets/gallery/06.jpg', 'w_560,f_webp,q_85')} type="image/webp" />
-            <img src={getPhotoUrl('/assets/gallery/06.jpg', 'w_560,q_85')} />
+            <source src={getPhotoUrl('assets/gallery/06.jpg', 'w_560,f_webp,q_85')} type="image/webp" />
+            <img src={getPhotoUrl('assets/gallery/06.jpg', 'w_560,q_85')} />
         </picture>
         <picture>
-            <source src={getPhotoUrl('/assets/gallery/07.jpg', 'w_560,f_webp,q_85')} type="image/webp" />
-            <img src={getPhotoUrl('/assets/gallery/07.jpg', 'w_560,q_85')} />
+            <source src={getPhotoUrl('assets/gallery/07.jpg', 'w_560,f_webp,q_85')} type="image/webp" />
+            <img src={getPhotoUrl('assets/gallery/07.jpg', 'w_560,q_85')} />
         </picture>
         <picture>
-            <source src={getPhotoUrl('/assets/gallery/08.jpg', 'w_560,f_webp,q_85')} type="image/webp" />
-            <img src={getPhotoUrl('/assets/gallery/08.jpg', 'w_560,q_85')} />
+            <source src={getPhotoUrl('assets/gallery/08.jpg', 'w_560,f_webp,q_85')} type="image/webp" />
+            <img src={getPhotoUrl('assets/gallery/08.jpg', 'w_560,q_85')} />
         </picture>
 
         <section class={style.content}>
