19a4156886c6544c4803aa04f1daff9f6c75b148
Arnelle Balane <arnellebalane@gmail.com>
2020-03-28 01:21:24 +0800

    Add link to livestream

--- src/components/hero-block/index.js
+++ src/components/hero-block/index.js
@@ -3,6 +3,9 @@ import ButtonLink from '../button-link';
 import logo from '../../images/wtm-logo.svg';
 import event from '../../data/event.json';
 
+const raffleUrl = 'https://gleam.io/8jbla/wtm-cebu-international-womens-day-online-summit';
+const livestreamUrl = 'https://www.youtube.com/watch?v=a-4x-MaoIwA';
+
 const HeroBlock = () => (
 	<section class={style.hero}>
 		<div class={style.details}>
@@ -12,7 +15,8 @@ const HeroBlock = () => (
 			<h3>{event.date} <span>&middot;</span> {event.venue}</h3>
 
 			<ButtonLink className={style.button} href={event.register_url}>Register now</ButtonLink>
-			<ButtonLink className={style.button} href="https://gleam.io/8jbla/wtm-cebu-international-womens-day-online-summit">Join our raffle</ButtonLink>
+			<ButtonLink className={style.button} href={raffleUrl}>Join our raffle</ButtonLink>
+			<ButtonLink className={style.button} href={livestreamUrl}>Watch us live!</ButtonLink>
 		</div>
 	</section>
 );
